4808612107df4895870c5ea8fbc748a7fb5f8f12
eugenio <eugenio.varetti@mail.polimi.it>
2026-03-26 22:06:32 +0100

    init commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/visualizer-prosperity-4/',
+  build: {
+    minify: false,
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
+});
